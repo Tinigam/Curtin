8697375133688be66cf2743821cac7542a6f1772
Yuyang Gao <maginit75@gmail.com>
2023-03-14 11:22:33 +0800

    add main() for hanoi

--- Trimester1A2023/DSA1002/Practice/Prectice02/Activity5.py
+++ Trimester1A2023/DSA1002/Practice/Prectice02/Activity5.py
@@ -13,4 +13,12 @@ def hanoi(n, src, temp, dest):
         moveDisk(n, src, dest)
         hanoi(n - 1, temp, src, dest)
 
-hanoi(10, "A", "B", "C")
+def main():
+    inValn = input("Please enter the number of disks:\n")
+    inValsrc = input("Please enter the name of source peg:\n")
+    inValtemp = input("Please enter the name of tempory peg:\n")
+    inValdest = input("Please enter the name of destination peg\n")
+    hanoi(inValn, inValsrc, inValtemp, inValdest)
+
+main()
+# hanoi(4, "A", "B", "C")
